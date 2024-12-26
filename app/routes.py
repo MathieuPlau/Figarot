2cf215c0ed0943f35764a4378a38569cd45267b5
Mathieu Plau <mathieu.plau@sogilis.com>
2024-12-26 12:02:31 +0100

    Menage et font

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@main_bp.route('/welcome')
 def home():
     return render_template('home.html')
 
@@ -13,7 +13,7 @@ def home():
 def about():
     return render_template('about.html')
 
-@main_bp.route('/sounds')
+@main_bp.route('/')
 def sounds():
     # Serve the main page with tabs for folders
     directory_path = request.args.get('dir', Config.SAMPLES_PATH)
@@ -26,31 +26,6 @@ def folder_contents():
     folder_path = request.args.get('folder')
     folder_contents = parse_files(Config.SAMPLES_PATH + folder_path)  # Get only files in the folder
     return jsonify(folder_contents)
-
-
-# @main_bp.route('/sounds')
-# def sounds():
-#     directory_path = request.args.get('dir', Config.SAMPLES_PATH)  # default to current directory
-#     directory_contents = parse_directory(directory_path)
-#     return render_template('sounds.html', directory_contents=directory_contents, directory_path=directory_path)
-
-# @main_bp.route('/wav')
-# def soundtest():
-#     sound_path = Path(Config.SAMPLES_PATH) / "Bobo.wav"
-
-#     if sound_path.exists():
-#         try:
-#             play_wave(str(sound_path))  # Play sound on the server
-#             return jsonify({"status": "success", "message": "Sound played successfully"})
-#         except Exception as e:
-#             return jsonify({"status": "error", "message": str(e)}), 500
-#     else:
-#         return jsonify({"status": "error", "message": "File not found"}), 404
-
-@main_bp.route('/wav')
-def soundtest():
-    sound_path = Path(Config.SAMPLES_PATH) / "Bobo.wav"
-    return play_wave(str(sound_path))
 
 # Play a given sound
 @main_bp.route('/play_sound', methods=['POST'])
