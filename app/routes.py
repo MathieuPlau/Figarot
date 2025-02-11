4981943efca3fce4e40f3713ce9b945cd862d815
MPL <mathieu.plau@yahoo.com>
2025-02-11 22:49:39 +0100

    Cleanup

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,6 @@ import threading
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/welcome')
-def home():
-    return render_template('home.html')
-
-@main_bp.route('/about')
-def about():
-    return render_template('about.html')
-
 @main_bp.route('/')
 def sounds():
     # Serve the main page with tabs for folders
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SAMPLES_PATH = "./samples/"
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = True
     # Add other configuration options as needed
